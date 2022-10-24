b9faa9359be6cee8cd62c4934b999d63c194c428
CodeBreaker44 <89404773+CodeBreaker44@users.noreply.github.com>
2022-10-24 13:59:59 +0300

    fixed death animation

--- js/classes.js
+++ js/classes.js
@@ -150,7 +150,7 @@ class Zaraki extends Sprite {
     }
 
     takeHit() {
-        this.health -= 5
+        this.health -= 20
 
         if (this.health <= 0) {
             this.switchSprite('death')
